964e9aa6caf3a2e1d4ba5f7aa116598753bebbba
Anthony Muli <mulianthony561@gmail.com>
2020-04-05 21:33:39 +0300

    Registered the models to be viewed in the admin sector

--- EPAD/epad/admin.py
+++ EPAD/epad/admin.py
@@ -4,7 +4,8 @@ from .models import *
 
 # Register your models here.
 class TaskAdmin(admin.ModelAdmin):
-    filter_horizontal=('assigned_to')
+    filter_horizontal=('assigned_to',)
+    
 admin.site.register(profile)
 admin.site.register(tasks,TaskAdmin)
 admin.site.register(kpis)
